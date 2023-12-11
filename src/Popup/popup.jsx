222b5c70c65f1ae632de8db20b22d67e991a295d
Dwight <dwightdoane@me.com>
2023-12-11 10:05:17 -0500

    hello popup

---
+++ src/Popup/popup.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("app"));
+
+root.render(<div>Hello React!</div>);
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const defaultConfig = {
+  build: {
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        popup: "src/Popup/popup.jsx",
+      },
+      output: {
+        assetFileNames: (asset) => {
+          switch (asset.name) {
+            case "popup":
+              return "/popup/[name].[ext]";
+            default:
+              return "[name].[ext]";
+          }
+        },
+        entryFileNames: (chunk) => {
+          switch (chunk.name) {
+            case "popup":
+              return "popup/[name].js";
+
+            default:
+              return "[name].js";
+          }
+        },
+      },
+    },
+  },
+
+  plugins: [react()],
+};
+
+export default defineConfig(() => defaultConfig);
